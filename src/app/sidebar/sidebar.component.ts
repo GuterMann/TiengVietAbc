4e246642c912020922f3e3e94a99406a5fc37b54
Le Minh Bui <leminh.bui@labs.lupusalpha.de>
2018-03-02 09:33:37 +0100

    Selektoren für Komponenten umbenannt. Cmp entfernt und dafür app- als Präfix.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var $:any;
+declare var $: any;
 
 export interface RouteInfo {
     path: string;
@@ -11,18 +11,17 @@ export interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
     { path: 'dashboard', title: 'Dashboard',  icon: 'ti-panel', class: '' },
-    { path: 'user', title: 'User Profile',  icon:'ti-user', class: '' },
-    { path: 'table', title: 'Table List',  icon:'ti-view-list-alt', class: '' },
-    { path: 'typography', title: 'Typography',  icon:'ti-text', class: '' },
-    { path: 'icons', title: 'Icons',  icon:'ti-pencil-alt2', class: '' },
-    { path: 'maps', title: 'Maps',  icon:'ti-map', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon:'ti-bell', class: '' },
-    { path: 'upgrade', title: 'Upgrade to PRO',  icon:'ti-export', class: 'active-pro' },
+    { path: 'user', title: 'User Profile',  icon: 'ti-user', class: '' },
+    { path: 'table', title: 'Table List',  icon: 'ti-view-list-alt', class: '' },
+    { path: 'typography', title: 'Typography',  icon: 'ti-text', class: '' },
+    { path: 'icons', title: 'Icons',  icon: 'ti-pencil-alt2', class: '' },
+    { path: 'maps', title: 'Maps',  icon: 'ti-map', class: '' },
+    { path: 'notifications', title: 'Notifications',  icon: 'ti-bell', class: '' },
 ];
 
 @Component({
     moduleId: module.id,
-    selector: 'sidebar-cmp',
+    selector: 'app-sidebar',
     templateUrl: 'sidebar.component.html',
 })
 
@@ -31,8 +30,8 @@ export class SidebarComponent implements OnInit {
     ngOnInit() {
         this.menuItems = ROUTES.filter(menuItem => menuItem);
     }
-    isNotMobileMenu(){
-        if($(window).width() > 991){
+    isNotMobileMenu() {
+        if ($(window).width() > 991) {
             return false;
         }
         return true;
